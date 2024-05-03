77a4cec560793baf855955fee19ac58aa25d91ce
agnaldo123 <105053109+agnaldo123@users.noreply.github.com>
2024-05-03 09:57:31 -0300

    .

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-04T07:00:00");
 const tempoObjetivo2 = new Date("2025-04-19T07:00:00");
 const tempoObjetivo3 = new Date("2024-07-03T07:00:00");
-const tempoObjetivo4 = new Date("2025-10-14T07:00:00");
+const tempoObjetivo4 = new Date("2024-10-14T07:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
